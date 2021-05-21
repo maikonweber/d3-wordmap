997bebba2edfd01e786a4823fe504c545911d7a7
Maikon <maikonweber@gmail.com>
2021-05-21 06:57:36 -0300

    d2 word map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import { json, geoEqualEarth, geoPath } from 'd3';
+import { feature } from 'topojson'
+
+const jsonUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  const width = 960;
+  const height = 500;
+
+  const projection = geoEqualEarth()
+    .scale(160)
+    .translate([800/2 , 450/2])
+
+  useEffect(() => {
+    json(jsonUrl).then(topojsonData => {
+      setData(feature(topojsonData, topojsonData.objects.countries).features);
+      
+
+    })
+
+
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      <g className="marks">
+        { data.map((d, i) => (
+         <path 
+         key = {`path-${i}`}
+         d={ geoPath().projection(projection)(d) }
+
+        
+        />
+        ))
+}
+      </g>
+    </svg>
   );
 }
 
